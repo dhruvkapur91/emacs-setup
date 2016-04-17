c3abd9248ddbcde35a762997d8dbb77e87958acd
Dhruv <dhruvkapur91@gmail.com>
2016-04-18 03:42:16 +0530

    Stop the irritating bell. Format

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Hoping to see a visible bell instead of sound one
+(setq visible-bell nil)
+(setq ring-bell-function (lambda ()
+			   (invert-face 'mode-line)
+			   (run-with-timer 0.1 nil 'invert-face 'mode-line)))
+
 ;;Setting default directory for emacs
 (setq default-directory "/Users/dhruvk/Projects")
 
@@ -98,22 +104,22 @@
 ;; Configuring eshell to open from a buffer
 
 (defun eshell-here ()
-    "Opens up a new shell in the directory associated with the
+  "Opens up a new shell in the directory associated with the
 current buffer's file. The eshell is renamed to match that
 directory to make multiple eshell windows easier."
-    (interactive)
-    (let* ((parent (if (buffer-file-name)
-		       (file-name-directory (buffer-file-name))
-		     default-directory))
-	   (height (/ (window-total-height) 3))
-	   (name   (car (last (split-string parent "/" t)))))
-      (split-window-vertically (- height))
-      (other-window 1)
-      (eshell "new")
-      (rename-buffer (concat "*eshell: " name "*"))
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+		     (file-name-directory (buffer-file-name))
+		   default-directory))
+	 (height (/ (window-total-height) 3))
+	 (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
 
-      (insert (concat "ls"))
-      (eshell-send-input)))
+    (insert (concat "ls"))
+    (eshell-send-input)))
 
 (global-set-key (kbd "C-c C-e") 'eshell-here)
 
