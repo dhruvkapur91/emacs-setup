67b5949aff9cf54834f02dadd9a7c07f043b32e0
Dhruv <dhruvkapur91@gmail.com>
2016-04-18 03:00:15 +0530

    Add path to ESS file and load ESS. Version seems to be 15.09

--- init.el
+++ init.el
@@ -150,3 +150,11 @@ directory to make multiple eshell windows easier."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; For ESS
+
+(add-to-list 'load-path "/usr/local/self/ESS-15.09/lisp/")
+(load "ess-site")
+
