da1d7ccaad769e9363e7522a834c689c258ab988
Dhruv <dhruvkapur91@gmail.com>
2016-04-20 11:09:55 +0530

    Add full screen magit status

--- init.el
+++ init.el
@@ -60,6 +60,21 @@
 ;; (package 'magit) ;; currently doesn't work. don't know why. TODO
 ;; So currently its installed with package-install magit
 
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 ;; magit-status can be called through Mx.
 ;; s to stage a file
 ;; k to checkout a file
